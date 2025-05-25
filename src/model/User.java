f81524db62615252c82034cb9788de3c45c05a7f
Sandhya Sapkota <sandhyasapkota64@gmail.com>
2025-05-25 16:57:21 +0545

    Login controller added

---
+++ src/model/User.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sandhya sapkota
+ */
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String security_ans;
+    
+    public User(String username, String email,String password,String security_ans){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.security_ans = security_ans;
+    }
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+        public User(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getSecurity_ans() {
+        return security_ans;
+    }
+    public void setSecurity_ans(String security_ans) {
+        this.security_ans = security_ans;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    public int getId() {
+        return id;
+    }
+}
+
+
+
